d085feda0f9eadb4f7e80e04dc87a12ddbfbfc0b
jaliba-sherin96 <jalibasherin@zohomail.in>
2026-06-04 14:46:24 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: " Attention Economy Revenue Simulator",
-  description: "Visualize how user time and platform impressions translate into revenue generation.",
+  title: "Attention Economy Revenue Simulator",
+  description: "Visualize how user time and platform impressions translate into revenue generation.",
 };
 
 export default function RootLayout({
-  children,
+  children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
